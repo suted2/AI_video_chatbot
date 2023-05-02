f8b69ea7d53354a73e3b91ef8f09ac86ed445a7b
doh0106 <121469539+doh0106@users.noreply.github.com>
2023-05-02 16:19:25 +0900

    Create select_data.py

---
+++ preprocessing/select_data.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+import numpy as np 
+import os 
+import glob 
+import cv2 
+import json
+from tqdm.notebook import tqdm
+import matplotlib.pyplot as plt
+from moviepy.video.io.VideoFileClip import VideoFileClip
+import tarfile
